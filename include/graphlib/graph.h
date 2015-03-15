aea7c37d553fae4c8e5aa241a1baddf3fefac7c2
Kang Min Yoo <kaniblurous@gmail.com>
2015-03-15 11:59:27 +0900

    updated

--- include/graphlib/graph.h
+++ include/graphlib/graph.h
@@ -6,66 +6,69 @@
 #include <set>
 #include <unordered_set>
 
-template <typename V> class graph
+namespace graphlib
 {
-private:
-	int _vertex_size;
-	int _edge_size;
-	std::unordered_set<V> _vertices_set;
+    template<typename V> class graph
+    {
+    private:
+        int _vertex_size;
+        int _edge_size;
+        std::unordered_set<V> _vertices_set;
 
-public:
-	std::vector<V> vertices;
-	std::unordered_map<V, std::set<V>> out_vertices;
-	std::unordered_map<V, std::set<V>> in_vertices;
-	
-	void add_vertex(const V &vertex)
-	{
-		if (_vertices_set.find(vertex) == _vertices_set.end()) {
-			_vertices_set.emplace(vertex);
-			vertices.push_back(vertex);
-			++_vertex_size;
-		}
-	}
+    public:
+        std::vector<V> vertices;
+        std::unordered_map<V, std::set<V>> out_vertices;
+        std::unordered_map<V, std::set<V>> in_vertices;
 
-	void add_edge(const V &from, const V &to)
-	{
-		add_vertex(from);
-		add_vertex(to);
+        void add_vertex(const V &vertex)
+        {
+            if (_vertices_set.find(vertex) == _vertices_set.end()) {
+                _vertices_set.emplace(vertex);
+                vertices.push_back(vertex);
+                ++_vertex_size;
+            }
+        }
 
-		if (out_vertices.find(from) == out_vertices.end())
-			out_vertices.emplace(from, std::set<V>());
-	
-		if (in_vertices.find(to) == in_vertices.end())
-			in_vertices.emplace(to, std::set<V>());
+        void add_edge(const V &from, const V &to)
+        {
+            add_vertex(from);
+            add_vertex(to);
 
-		if (out_vertices[from].find(to) == out_vertices[from].end()) {
-			++_edge_size;
-			out_vertices[from].emplace(to);
-		}
+            if (out_vertices.find(from) == out_vertices.end())
+                out_vertices.emplace(from, std::set<V>());
 
-		if (in_vertices[to].find(from) == in_vertices[to].end())
-			in_vertices[to].emplace(from);
-	}
+            if (in_vertices.find(to) == in_vertices.end())
+                in_vertices.emplace(to, std::set<V>());
 
-	int vertex_size() const
-	{
-		return _vertex_size;
-	}
+            if (out_vertices[from].find(to) == out_vertices[from].end()) {
+                ++_edge_size;
+                out_vertices[from].emplace(to);
+            }
 
-	int edge_size() const
-	{
-		return _edge_size;
-	}
+            if (in_vertices[to].find(from) == in_vertices[to].end())
+                in_vertices[to].emplace(from);
+        }
 
-	std::set<V> get_out_vertices(const V &vertex) const
-	{
-		return out_vertices.find(vertex)->second;
-	}
+        int vertex_size() const
+        {
+            return _vertex_size;
+        }
 
-	std::set<V> get_in_vertices(const V &vertex) const
-	{
-		return in_vertices.find(vertex)->second;
-	}
-};
+        int edge_size() const
+        {
+            return _edge_size;
+        }
 
+        std::set<V> get_out_vertices(const V &vertex) const
+        {
+            return out_vertices.find(vertex)->second;
+        }
+
+        std::set<V> get_in_vertices(const V &vertex) const
+        {
+            return in_vertices.find(vertex)->second;
+        }
+    };
+
+}
 #endif
--- include/graphlib/graph_algorithms.h
+++ include/graphlib/graph_algorithms.h
@@ -5,42 +5,42 @@
 #include <cmath>
 #include "graph.h"
 
-namespace graph 
+namespace graphlib
 {
-	template<class T> void ppr(const graph<T> &graph, const T &root, std::unordered_map<T, double> &out_ranks, const double damping = 0.85, const double epsilon = 0.001, const int iteration = 20)
-	{
-		out_ranks.clear();
-		double delta = 1;
-		double count = 0;
+    template<class T> void ppr(const graph<T> &graph, const T &root, std::unordered_map<T, double> &out_ranks, const double damping = 0.85, const double epsilon = 0.001, const int iteration = 20)
+    {
+        out_ranks.clear();
+        double delta = 1;
+        double count = 0;
 
-		for (auto it = graph.vertices.begin(); it != graph.vertices.end(); ++it)
-			out_ranks.emplace(*it, 1.0/graph.vertex_size());
+        for (auto it = graph.vertices.begin(); it != graph.vertices.end(); ++it)
+            out_ranks.emplace(*it, 1.0 / graph.vertex_size());
 
-		while (delta > epsilon && count != iteration) {
-			delta = 0;
+        while (delta > epsilon && count != iteration) {
+            delta = 0;
 
-			for (auto it = graph.vertices.begin(); it != graph.vertices.end(); ++it) {
-				double sum = 0;
+            for (auto it = graph.vertices.begin(); it != graph.vertices.end(); ++it) {
+                double sum = 0;
 
-				auto it3 = graph.in_vertices.find(*it);
-				if (it3 != graph.in_vertices.end()) {
-					for (auto it2 = it3->second.begin(); it2 != it3->second.end(); ++it2)
-						sum += out_ranks[*it2] / graph.out_vertices.find(*it2)->second.size();
-				}
+                auto it3 = graph.in_vertices.find(*it);
+                if (it3 != graph.in_vertices.end()) {
+                    for (auto it2 = it3->second.begin(); it2 != it3->second.end(); ++it2)
+                        sum += out_ranks[*it2] / graph.out_vertices.find(*it2)->second.size();
+                }
 
-				const double old_rank = out_ranks[*it];
-				out_ranks[*it] = sum * damping + (root == *it ? 1 : 0) * (1 - damping);
-				delta += abs(out_ranks[*it] - old_rank);
-			}
+                const double old_rank = out_ranks[*it];
+                out_ranks[*it] = sum * damping + (root == *it ? 1 : 0) * (1 - damping);
+                delta += abs(out_ranks[*it] - old_rank);
+            }
 
-			++count;
-		}
-	}
+            ++count;
+        }
+    }
 
-	template <typename T> void betweenness(const graph<T> &graph)
-	{
+    template<typename T> void betweenness(const graph<T> &graph)
+    {
 
-	}
+    }
 }
 
 #endif
